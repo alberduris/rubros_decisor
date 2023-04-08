4dc58b03c9c76e94208aaa2b5ead265ac181d471
alberduris <4lb3rduris@gmail.com>
2023-04-08 13:43:52 +0200

    fixed things

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 from langchain.llms import OpenAI
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
@@ -13,34 +14,31 @@ from openai.embeddings_utils import get_embedding
 import openai
 import json
 from utils import detect_entities, similarity_search_threshold, unspecificity_detector, rubro_decisor, unspecificity_explainer
+
 openai.api_key = "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"
 
-def download_faiss_index(set_name):
-    url = f'https://raw.githubusercontent.com/your_username/your_repository_name/main/indexes/{set_name}_rubros_index'
-    response = requests.get(url)
-    filename = f'indexes/{set_name}_rubros_index'
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-    return filename
 
 db = {}
 for set in ['all', 'curated']:
     # Load the FAISS DB
-    local_path = download_faiss_index(set)
-    db[set] = FAISS.load_local(local_path, OpenAIEmbeddings(openai_api_key=openai.api_key))
+    pass
+    db[set] = FAISS.load_local(
+        f"indexes/{set}_rubros_index", OpenAIEmbeddings(openai_api_key=openai.api_key))
 
 st.title('Rubro Decider App')
 
-client_text = st.text_input('Enter a client\'s text:', 'Marca de decoración hecha a mano')
+client_text = st.text_input(
+    'Enter a client\'s text:', 'Marca de decoración hecha a mano')
 
 if st.button('Submit'):
     entities = detect_entities(client_text)
     rubros = pd.Series()
     for ent in entities:
-        rubros = pd.concat([rubros, similarity_search_threshold(db['curated'], ent, 0.3, 25)['page_content']])
+        rubros = pd.concat([rubros, similarity_search_threshold(
+            db['curated'], ent, 0.3, 25)['page_content']])
     if rubros.empty:
-        rubros = pd.Series(similarity_search_threshold(db['all'], client_text, 0.3, 25)['page_content'])
+        rubros = pd.Series(similarity_search_threshold(
+            db['all'], client_text, 0.3, 25)['page_content'])
     rubros = list(rubros.values)
 
     nl = '\n- '
